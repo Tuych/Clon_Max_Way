454579709ce04179400a63222479873723709618
Tuych <tuychiyevbaxtiyor06@gmail.com>
2024-03-08 23:13:07 +0300

    add urls

--- src/max_way/settings.py
+++ src/max_way/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'max_way.base',
+    'max_way.product',
+    'max_way.user',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'max_way.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
